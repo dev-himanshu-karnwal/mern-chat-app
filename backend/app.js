1da46e30030f03b71a41b62dbcabcde03d2f2546
Himanshu <himanshukar1810@gmail.com>
2023-10-29 23:23:01 +0530

    implemented get chat route, multiple message routes, and grp

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,11 @@ const morgan = require("morgan");
 
 const userRouter = require(path.join(__dirname, "./routes/user-routes.js"));
 const groupRouter = require(path.join(__dirname, "./routes/group-routes.js"));
+const chatRouter = require(path.join(__dirname, "./routes/chat-routes.js"));
+const messageRouter = require(path.join(
+  __dirname,
+  "./routes/message-routes.js"
+));
 const globalErrorHandler = require(path.join(
   __dirname,
   "./controllers/error-controller"
@@ -16,7 +21,9 @@ app.use(express.json());
 app.use(morgan("dev"));
 
 app.use("/api/v1/users", userRouter);
+app.use("/api/v1/chats", chatRouter);
 app.use("/api/v1/groups", groupRouter);
+app.use("/api/v1/messages", messageRouter);
 
 app.use("*", (req, res, next) =>
   next(new AppError(`${req.originalUrl} : Not found / Invalid URL`, 404))
