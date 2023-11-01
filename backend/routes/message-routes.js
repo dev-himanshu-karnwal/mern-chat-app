200641dc806f41d01e5d56c45a3546816a293e52
Himanshu <himanshukar1810@gmail.com>
2023-11-01 13:23:32 +0530

    Added route to get group and chat messages

--- backend/routes/message-routes.js
+++ backend/routes/message-routes.js
@@ -19,6 +19,7 @@ router
   .delete(messageController.completelyDeleteMessage)
   .patch(messageController.deleteMessage);
 
-// TODO: get all messages of a particular chat or group
+router.get("/user/:id", messageController.getChatMessages);
+router.get("/group/:id", messageController.getGroupMessages);
 
 module.exports = router;
